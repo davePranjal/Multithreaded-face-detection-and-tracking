b224116f24aa6efc1bb4982f38d3fbaaab511aaf
davePranjal <pranjaldave8@gmail.com>
2020-05-10 13:12:56 +0530

    Added README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Test  
+Currently contains a compilation of code implementing each part of desired result.
+
+Output.avi - Output of face_detection.py
+
+testVid.mp4 - Input Video 
---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  8 23:48:40 2020
+
+@author: pranj
+"""
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import face_recognition
