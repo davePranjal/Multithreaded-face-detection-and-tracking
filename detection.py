54329b9503390f219c64bf944117ff38ad42b4e9
davePranjal <pranjaldave8@gmail.com>
2020-05-11 14:54:11 +0530

    Added to main.py
    The code is currently able to detect the faces. But the detection is happening on each frame slowing the video.

--- detection.py
+++ detection.py
@@ -4,19 +4,18 @@ import numpy as np
 import cv2
 
 face_cascade = cv2.CascadeClassifier('C:\\Users\\pranj\\anaconda\\pkgs\\libopencv-4.2.0-py37_6\\Library\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('C:\\Users\\pranj\\anaconda\\pkgs\\libopencv-4.2.0-py37_6\\Library\\etc\\haarcascades\\haarcascade_eye.xml')
 
 img = cv2.imread('speaker1.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+print(tuple(faces))
 for (x,y,w,h) in faces:
+    print(tuple((x,y,w,h)))
+    print((x,y,w,h))
     img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
 cv2.imshow('img',img)
-cv2.waitKey(0)
+#cv2.waitKey(0)
 cv2.destroyAllWindows()
